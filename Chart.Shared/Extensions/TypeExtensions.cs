35446ca2bd9b36bd781b575acdbf8970f422132d
Max <madstk1@pm.me>
2023-03-03 16:24:56 +0100

    refactor(ModelParser): move reflection extensions to Chart.Shared

--- Chart.Shared/Extensions/TypeExtensions.cs
+++ Chart.Shared/Extensions/TypeExtensions.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace Chart.Shared.Extensions
 {
     public static class TypeExtensions
@@ -9,5 +11,43 @@ namespace Chart.Shared.Extensions
         /// <returns>True, if the type is an IEnumerable. Otherwise, false.</returns>
         public static bool IsEnumerable(this Type type)
             => typeof(System.Collections.IEnumerable).IsAssignableFrom(type);
+
+            /// <summary>
+        /// Get all properties of a type parameter.
+        /// </summary>
+        /// <param name="type">The type to retrieve properties from.</param>
+        /// <returns>List of PropertyInfo-objects.</returns>
+        public static List<PropertyInfo> GetLocalProperties(this Type type)
+            => type.GetProperties(
+                    BindingFlags.Instance |
+                    BindingFlags.Public)
+                .ToList();
+
+        /// <summary>
+        /// Get all fields of a type parameter.
+        /// </summary>
+        /// <param name="type">The type to retrieve properties from.</param>
+        /// <returns>List of FieldInfo-objects.</returns>
+        public static List<FieldInfo> GetLocalFields(this Type type)
+            => type.GetFields(
+                    BindingFlags.Instance |
+                    BindingFlags.Public)
+                .ToList();
+
+        /// <summary>
+        /// Get all methods of a type parameter.
+        /// </summary>
+        /// <remarks>
+        /// Property getters/setters are not included.
+        /// </remarks>
+        /// <param name="type">The type to retrieve properties from.</param>
+        /// <returns>List of MethodInfo-objects.</returns>
+        public static List<MethodInfo> GetLocalMethods(this Type type)
+            => type.GetMethods(
+                    BindingFlags.DeclaredOnly |
+                    BindingFlags.Instance |
+                    BindingFlags.Public)
+                .Where(m => !m.IsSpecialName)
+                .ToList();
     }
 }
